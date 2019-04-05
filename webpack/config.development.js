9ef643777bea6ed8abf47bc1c98e0874a61abb5d
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2019-04-05 20:06:13 +0700

    change port for project

--- webpack/config.development.js
+++ webpack/config.development.js
@@ -17,7 +17,7 @@ const devConf = {
     inline: true,
     historyApiFallback: true,
     host: '0.0.0.0',
-    port: 3000,
+    port: 3001,
     open: false,
     openPage: '',
     contentBase: ['build'],
