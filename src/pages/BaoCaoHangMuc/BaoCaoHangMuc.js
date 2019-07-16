9f933491e78607d82d0faff50dbc97eb675b0dc2
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2019-07-16 23:26:13 +0700

    add upload

--- src/pages/BaoCaoHangMuc/BaoCaoHangMuc.js
+++ src/pages/BaoCaoHangMuc/BaoCaoHangMuc.js
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react'
-import { PageHeader, Table, Divider, Row, Col, Button } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Button, Form, Upload, Icon } from 'antd'
 // import TaoMoiDonViChuQuan from './TaoMoi'
 // import ChiTietDonViChuQuan from './ChiTiet'
 
-export default function DonViChuQuan ({
+const { Item } = Form
+const { Dragger } = Upload
+
+function DonViChuQuan ({
+  form,
   donViChuQuanList,
   donViChuQuanEditing,
   getDonViChuQuan,
@@ -12,84 +16,122 @@ export default function DonViChuQuan ({
   addDonViChuQuan,
   deleteDonViChuQuan
 }) {
-  const { Column } = Table
+  // const { Column } = Table
+  const [file, setFile] = useState()
   // const [addVisible, setAddVisible] = useState(false)
   // const [editVisible, setEditVisible] = useState(false)
   useEffect(() => {
     // getDonViChuQuan()
   }, [])
 
-  const dataTable =
-    donViChuQuanList &&
-    donViChuQuanList.map((item, index) => ({
-      key: item.id,
-      ct: index,
-      name: item.name,
-      soLuongTieuChi: item.soLuongTieuChi
-    }))
+  // const dataTable =
+  //   donViChuQuanList &&
+  //   donViChuQuanList.map((item, index) => ({
+  //     key: item.id,
+  //     ct: index,
+  //     name: item.name,
+  //     soLuongTieuChi: item.soLuongTieuChi
+  //   }))
+
+  const onChangeFile = file => {
+    setFile(file)
+    return false
+  }
+
+  const resetFileField = e => {
+    if (Array.isArray(e)) {
+      return e
+    }
+    return e && e.fileList
+  }
 
   return (
     <>
-      <PageHeader title='[Tên Mẫu Hạng Mục Báo Cáo]' />
+      <Form>
+        <Item label='Upload Image'>
+          {form.getFieldDecorator('file', {
+            valuePropName: 'fileList',
+            getValueFromEvent: resetFileField,
+            rules: [
+              { required: true, message: '画像ファイルを選択してください' }
+            ]
+          })(
+            <Dragger
+              beforeUpload={onChangeFile}
+              listType='picture'
+              multiple={false}
+            >
+              <p className='ant-upload-drag-icon'>
+                <Icon type='inbox' />
+              </p>
+              <p className='ant-upload-text'>
+                Click or drag file to this area to upload
+              </p>
+              <p className='ant-upload-hint'>Support for a single upload.</p>
+            </Dragger>
+          )}
+        </Item>
+      </Form>
       <Row>
         <Col col={4} offset={21}>
           <Button
             style={{ marginBottom: '20px' }}
             type='primary'
-            icon='plus'
-            // onClick={() => {
-            //   setAddVisible(true)
-            // }}
+            onClick={() => {
+              const formData = new FormData()
+              formData.append('file', file)
+            }}
           >
-            Tạo mới
+            Send
           </Button>
         </Col>
       </Row>
-      <Table dataSource={dataTable}>
-        <Column title='CT' dataIndex='ct' key='ct' />
-        <Column title='Mã' dataIndex='code' key='code' />
-        <Column title='Hạng Mục' dataIndex='hangMuc' key='hangMuc' />
-        <Column title='Tên Hạng Mục' dataIndex='name' key='name' />
-        <Column title='Số liệu' dataIndex='soLieu' key='soLieu' />
-        <Column
-          title=''
-          key='action'
-          render={(text, record) => (
-            <span>
-              <a
-                onClick={() => {
-                  // setEditVisible(true)
-                  getDonViChuQuanEditing(record.key)
-                }}
-              >
-                Cập nhật
-              </a>
-              <Divider type='vertical' />
-              <a
-                onClick={() => {
-                  deleteDonViChuQuan(record.key)
-                }}
-              >
-                Xoá
-              </a>
-            </span>
-          )}
-        />
-      </Table>
-      <Row style={{ marginTop: '20px' }}>
-        <Col col={4} offset={18}>
-          <Button style={{ marginBottom: '20px' }} type='primary' icon='plus'>
-            Lưu
-          </Button>
-          <Button
-            style={{ marginBottom: '20px', marginLeft: '20px' }}
-            type='primary'
-            icon='plus'
-          >
-            Duyệt
-          </Button>
-        </Col>
-      </Row>
+      {/* <PageHeader title='[Tên Mẫu Hạng Mục Báo Cáo]' /> */}
+      {/* <Table dataSource={dataTable}> */}
+      {/* <Column title='CT' dataIndex='ct' key='ct' /> */}
+      {/* <Column title='Mã' dataIndex='code' key='code' /> */}
+      {/* <Column title='Hạng Mục' dataIndex='hangMuc' key='hangMuc' /> */}
+      {/* <Column title='Tên Hạng Mục' dataIndex='name' key='name' /> */}
+      {/* <Column title='Số liệu' dataIndex='soLieu' key='soLieu' /> */}
+      {/* <Column */}
+      {/* title='' */}
+      {/* key='action' */}
+      {/* render={(text, record) => ( */}
+      {/* <span> */}
+      {/* <a */}
+      {/* onClick={() => { */}
+      {/* // setEditVisible(true) */}
+      {/* getDonViChuQuanEditing(record.key) */}
+      {/* }} */}
+      {/* > */}
+      {/* Cập nhật */}
+      {/* </a> */}
+      {/* <Divider type='vertical' /> */}
+      {/* <a */}
+      {/* onClick={() => { */}
+      {/* deleteDonViChuQuan(record.key) */}
+      {/* }} */}
+      {/* > */}
+      {/* Xoá */}
+      {/* </a> */}
+      {/* </span> */}
+      {/* )} */}
+      {/* /> */}
+      {/* </Table> */}
+      {/* <Row style={{ marginTop: '20px' }}> */}
+      {/* <Col col={4} offset={18}> */}
+      {/* <Button style={{ marginBottom: '20px' }} type='primary' icon='plus'> */}
+      {/* Lưu */}
+      {/* </Button> */}
+      {/* <Button */}
+      {/* style={{ marginBottom: '20px', marginLeft: '20px' }} */}
+      {/* type='primary' */}
+      {/* icon='plus' */}
+      {/* > */}
+      {/* Duyệt */}
+      {/* </Button> */}
+      {/* </Col> */}
+      {/* </Row> */}
       {/* <TaoMoiDonViChuQuan */}
       {/* addVisible={addVisible} */}
       {/* setAddVisible={setAddVisible} */}
@@ -98,3 +140,4 @@ export default function DonViChuQuan ({
     </>
   )
 }
+export default Form.create({ name: 'form_add_modal' })(DonViChuQuan)
